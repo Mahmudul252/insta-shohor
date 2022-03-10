875c5fc8133bc29f1fb778ba0828ba38d12b4a81
Mahmudul252 <mahmudulkabir990@gmail.com>
2022-03-10 11:29:51 -0600

    bug with user profile picture fixed

--- js/app.js
+++ js/app.js
@@ -15,7 +15,6 @@ const isLiked = (id) => {
   return likedPostsId?.length && !!likedPostsId.includes(id);
 };
 
-// bug 1 fixed
 const addToLiked = (id) => {
   likedPostsId.push(id);
   showPosts(posts);
@@ -52,7 +51,6 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
-  const image = post.image;
   const div = document.createElement("article");
   div.classList.add("post");
   div.innerHTML = `
@@ -63,7 +61,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${post.userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
@@ -77,7 +75,7 @@ const createPost = (post) => {
                 <div class="post__medias">
                   <img
                     class="post__media"
-                    src="${image}"
+                    src="${post.image}"
                     alt="Post Content"
                   />
                 </div>
